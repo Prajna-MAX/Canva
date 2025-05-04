aa6f3098b2b0a2315a9abf3be478eacc6ffca402
Prajna-MAX <152989672+Prajna-MAX@users.noreply.github.com>
2025-05-04 18:31:02 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:3001/login', {
+    axios.post('https://canva-qzw3.onrender.com/login', {
       email: formData.email,
       password: formData.password
     })
